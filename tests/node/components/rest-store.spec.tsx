6bc7280a45ffd6f70d448c405e52788680d6fd90
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-12-30 00:40:57 +0100

    Add test for post method

--- tests/node/components/rest-store.spec.tsx
+++ tests/node/components/rest-store.spec.tsx
@@ -55,7 +55,40 @@ describe('RestStore', () => {
       const transportLayer = Mock.ofType<TransportLayer>();
       transportLayer.setup(
         fake => fake.get<Foo[]>(':some-api:')
-      ).returns(() => Promise.resolve(data)).verifiable();
+      ).returns(() => Promise.resolve(data));
+
+      $render(<RestStore
+        View={Foo}
+        viewProps={viewProps}
+        api=":some-api:"
+        transportLayer={transportLayer.object}
+      />);
+
+      await wait(() => expect(Foo.renderedWith({
+        foo: 'bar',
+        data
+      })));
+    });
+
+    it('should post to the endpoint', async () => {
+      const Foo = createReactStub<FooProps>();
+      const viewProps = { foo: 'bar' };
+
+      const data = [{
+        id: 1,
+        title: 'some category'
+      }];
+
+      const newCategory = {
+        id: 2,
+        title: 'new category'
+      };
+
+      const transportLayer = Mock.ofType<TransportLayer>();
+
+      transportLayer.setup(
+        fake => fake.get<Foo[]>(':some-api:')
+      ).returns(() => Promise.resolve(data));
 
       $render(<RestStore
         View={Foo}
@@ -69,7 +102,19 @@ describe('RestStore', () => {
         data
       })));
 
-      transportLayer.verifyAll();
+      transportLayer.setup(
+        fake => fake.post<Foo>(':some-api:', {
+          title: 'new category'
+        })
+      ).returns(() => Promise.resolve(newCategory));
+
+      transportLayer.setup(
+        fake => fake.get<Foo[]>(':some-api:')
+      ).returns(() => Promise.resolve(data.concat([newCategory])));
+
+      await Foo.lastProps.post({ title: 'new category' });
+
+      await wait(() => expect(Foo.renderedWith({ data: data.concat([newCategory]) })));
     });
   });
 });
