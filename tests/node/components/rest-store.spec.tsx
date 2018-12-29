54ae1e47d8754b8682e8b5defa7d160f03a67198
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-12-29 20:37:21 +0100

    Improve test grouping

--- tests/node/components/rest-store.spec.tsx
+++ tests/node/components/rest-store.spec.tsx
@@ -15,55 +15,59 @@ interface FooProps extends RestViewProps<Foo>{
 }
 
 describe('RestStore', () => {
-  it('should render the loading view while fetching', async () => {
-    const Foo = createReactStub<FooProps>();
-    const Loading = createReactStub();
-    const viewProps = { foo: 'bar' };
+  describe('loading', () => {
+    it('should render the loading view', async () => {
+      const Foo = createReactStub<FooProps>();
+      const Loading = createReactStub();
+      const viewProps = { foo: 'bar' };
 
-    const transportLayer = Mock.ofType<TransportLayer>();
-    transportLayer.setup(
-      fake => fake.get<Foo[]>(':some-api:')
-    ).returns(() => new Promise(() => {})).verifiable();
+      const transportLayer = Mock.ofType<TransportLayer>();
+      transportLayer.setup(
+        fake => fake.get<Foo[]>(':some-api:')
+      ).returns(() => new Promise(() => {})).verifiable();
 
-    $render(<RestStore
-      View={Foo}
-      LoadingComponent={Loading}
-      viewProps={viewProps}
-      api=":some-api:"
-      transportLayer={transportLayer.object}
-    />);
+      $render(<RestStore
+        View={Foo}
+        LoadingComponent={Loading}
+        viewProps={viewProps}
+        api=":some-api:"
+        transportLayer={transportLayer.object}
+      />);
 
-    await wait(() => expect(Loading.rendered).to.be.true);
+      await wait(() => expect(Loading.rendered).to.be.true);
 
-    transportLayer.verifyAll();
+      transportLayer.verifyAll();
+    });
   });
 
-  it('should pass the response to the view', async () => {
-    const Foo = createReactStub<FooProps>();
-    const viewProps = { foo: 'bar' };
+  describe('loaded', () => {
+    it('should pass the response to the view', async () => {
+      const Foo = createReactStub<FooProps>();
+      const viewProps = { foo: 'bar' };
 
-    const data = [{
-      id: 1,
-      title: 'some category'
-    }];
+      const data = [{
+        id: 1,
+        title: 'some category'
+      }];
 
-    const transportLayer = Mock.ofType<TransportLayer>();
-    transportLayer.setup(
-      fake => fake.get<Foo[]>(':some-api:')
-    ).returns(() => Promise.resolve(data)).verifiable();
+      const transportLayer = Mock.ofType<TransportLayer>();
+      transportLayer.setup(
+        fake => fake.get<Foo[]>(':some-api:')
+      ).returns(() => Promise.resolve(data)).verifiable();
 
-    $render(<RestStore
-      View={Foo}
-      viewProps={viewProps}
-      api=":some-api:"
-      transportLayer={transportLayer.object}
-    />);
+      $render(<RestStore
+        View={Foo}
+        viewProps={viewProps}
+        api=":some-api:"
+        transportLayer={transportLayer.object}
+      />);
 
-    await wait(() => expect(Foo.renderedWith({
-      foo: 'bar',
-      data
-    })));
+      await wait(() => expect(Foo.renderedWith({
+        foo: 'bar',
+        data
+      })));
 
-    transportLayer.verifyAll();
+      transportLayer.verifyAll();
+    });
   });
 });
