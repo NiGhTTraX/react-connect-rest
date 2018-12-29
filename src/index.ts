95dfaa87d9789b1dc30c27319d6188358843721f
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-12-29 20:35:35 +0100

    Add publish script

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import connectToRest from './connect';
+import RestStore, { RestViewProps } from './rest-store';
+import LoadingSpinner from './loading-spinner';
+
+export default connectToRest;
+
+export { RestStore, LoadingSpinner, RestViewProps };
