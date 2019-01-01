7be11ad775d36762df2aed7cc846a48a5fb4b235
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-01-02 00:44:32 +0100

    Remove RHL from babel config

--- src/.babelrc.js
+++ src/.babelrc.js
@@ -4,8 +4,4 @@ if (process.env.COVERAGE) {
   config.plugins.push('istanbul');
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  config.plugins.push('react-hot-loader/babel');
-}
-
 module.exports = config;
