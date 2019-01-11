2fc3b48f3e708b0576c16b40af3f38115e0e5f1f
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-01-11 23:06:39 +0100

    Export RestStoreMock

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import connectToRest from './connect';
-import RestStore, { IRestStore } from './lib/rest-store';
+import RestStore, { IRestStore, RestStoreMock } from './lib/rest-store';
 
 export default connectToRest;
 
-export { RestStore, IRestStore };
+export { RestStore, IRestStore, RestStoreMock };
