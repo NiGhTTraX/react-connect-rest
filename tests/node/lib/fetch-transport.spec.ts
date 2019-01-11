9a0986ea150a7233cb436e7c924208ab5593f388
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-01-11 21:48:59 +0100

    Add tests for FetchTransport

---
+++ tests/node/lib/fetch-transport.spec.ts
@@ -0,0 +1,29 @@
+import fetchMock from 'fetch-mock';
+import { describe, expect, it } from '../suite';
+import FetchTransport from '../../../src/lib/fetch-transport';
+
+describe('FetchTransport', () => {
+  it('should make a GET request', async () => {
+    fetchMock.get('/api/', true);
+
+    expect(await FetchTransport.get<boolean>('/api/')).to.be.true;
+  });
+
+  it('should make a POST request', async () => {
+    const response = { id: 1, foo: 'bar' };
+    type T = { id: number, foo: string };
+
+    fetchMock.post(
+      (url, opts) => url === '/api/' && opts.body === JSON.stringify({ foo: 'bar' }),
+      response,
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    expect(await FetchTransport.post<T>('/api/', { foo: 'bar' })).to.deep.equal(response);
+  });
+});
