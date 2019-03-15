0be19120b573d4d5171bd58c0e2c8216310cf2ee
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-03-15 17:43:02 +0100

    Expose RestEntityStoreMock from index

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import { IRestCollectionStore, IRestEntityStore } from './lib/rest';
 import RestCollectionStore from './lib/rest-collection-store';
 import RestEntityStore from './lib/rest-entity-store';
 import RestCollectionStoreMock from './lib/rest-collection-store-mock';
+import RestEntityStoreMock from './lib/rest-entity-store-mock';
 
 export default connectToRest;
 
 export {
   RestCollectionStore, IRestCollectionStore,
   RestEntityStore, IRestEntityStore,
-  RestCollectionStoreMock
+  RestCollectionStoreMock, RestEntityStoreMock
 };
