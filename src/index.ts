c41bd500222c26cfa0b507fa4c8998e2e0c96344
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-03-22 19:54:53 +0100

    Expose HttpRestClient interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import connectToRest from './connect';
 import RestStore, { IRestStore, StoreModel } from './lib/rest-store';
 import RestStoreMock from './lib/rest-store-mock';
+import HttpRestClient, { PostPayload, PatchPayload, DeletePayload } from './lib/http-rest-client';
 
 export default connectToRest;
 
 export {
   RestStore, IRestStore, StoreModel,
+  HttpRestClient, PostPayload, PatchPayload, DeletePayload,
   RestStoreMock
 };
