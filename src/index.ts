c52fd8a73d03c163bdaadd003ee17fd86da41180
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-03-23 19:11:14 +0100

    Publish RestResponse type

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import connectToRest from './connect';
 import RestStore, { IRestStore, StoreModel } from './lib/rest-store';
 import RestStoreMock from './lib/rest-store-mock';
-import HttpRestClient, { PostPayload, PatchPayload, DeletePayload, RestModel } from './lib/http-rest-client';
+import HttpRestClient, { PostPayload, PatchPayload, DeletePayload, RestModel, RestResponse } from './lib/http-rest-client';
 
 export default connectToRest;
 
 export {
   RestStore, IRestStore, StoreModel,
-  HttpRestClient, PostPayload, PatchPayload, DeletePayload, RestModel,
+  HttpRestClient, PostPayload, PatchPayload, DeletePayload, RestModel, RestResponse,
   RestStoreMock
 };
